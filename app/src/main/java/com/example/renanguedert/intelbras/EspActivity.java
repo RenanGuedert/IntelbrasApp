0bd2a6d2782f87c4cbd05dbac57935de3a5ece75
Renan Guedert da Silva <renan.guedert@softplan.com.br>
2018-06-07 19:50:02 -0300

    inicio do projeto

---
+++ app/src/main/java/com/example/renanguedert/intelbras/EspActivity.java
@@ -0,0 +1,13 @@
+package com.example.renanguedert.intelbras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EspActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_esp);
+    }
+}
